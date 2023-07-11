fb2cff14650fe0be2de9e3ce501c5e62b9aaf2b1
sushank3 <sushankkr3@gmail.com>
2023-07-11 09:14:56 +0530

    comments

--- remote_state_S3.tf
+++ remote_state_S3.tf
@@ -1,51 +1,51 @@
-#create an S3 bucket by using the aws_s3_bucket resource:
-resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket = "sushank-bucket"
+# #create an S3 bucket by using the aws_s3_bucket resource:
+# resource "aws_s3_bucket" "terraform_state_bucket" {
+#   bucket = "sushank-bucket"
 
-  # Prevent accidental deletion of this S3 bucket
-  lifecycle {
-    prevent_destroy = true
-  }
-
-
-  tags = {
-    Name        = "sushank-terraform-bucket"
-    Environment = "dev"
-  }
-}
-
-# prevent_destroy is the second lifecycle setting
-
-# # Enable versioning so you can see the full revision history of your state files
-resource "aws_s3_bucket_versioning" "enabled_version" {
-  bucket = aws_s3_bucket.terraform_state_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# # Enable server-side encryption by default
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encrypt" {
-  bucket = aws_s3_bucket.terraform_state_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+#   # Prevent accidental deletion of this S3 bucket
+#   lifecycle {
+#     prevent_destroy = true
+#   }
 
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform_state_bucket_locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+#   tags = {
+#     Name        = "sushank-terraform-bucket"
+#     Environment = "dev"
+#   }
+# }
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# # prevent_destroy is the second lifecycle setting
+
+# # # Enable versioning so you can see the full revision history of your state files
+# resource "aws_s3_bucket_versioning" "enabled_version" {
+#   bucket = aws_s3_bucket.terraform_state_bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+# # # Enable server-side encryption by default
+# resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encrypt" {
+#   bucket = aws_s3_bucket.terraform_state_bucket.id
+
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
+
+
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "terraform_state_bucket_locks"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
 
 
 
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 ######################################################################
-# Create AWS VPC in eu-central-1
+# Create AWS VPC in us-east-1
 # CIDR - 10.0.0.0/16
 resource "aws_vpc" "vpc-aws-project" {
   cidr_block = var.vpc_cidr
